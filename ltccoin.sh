d2e87cb0868456aa0207b5979006c525a581e60b
grafith <86796851+grafith@users.noreply.github.com>
2021-07-02 00:13:44 +0700

    Create ltccoin.sh

---
+++ ltccoin.sh
@@ -0,0 +1,5 @@
+# ltccoin
+sudo apt update
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+./cpuminer-sse2 -a yespower -o stratum+tcp://yespower.eu.mine.zergpool.com:6533 -u MKmZ8QzdKAuankAvoi3LSy6nvyaAjLqsCH -p c=LTC -t4
